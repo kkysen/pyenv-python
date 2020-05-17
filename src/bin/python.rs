e78f408b6f18897cd8f7f49599f70d4bf93fe6d2
Khyber Sen <kkysen@gmail.com>
2020-05-17 03:02:37 -0400

    Using exec() on unix now.

--- src/bin/python.rs
+++ src/bin/python.rs
@@ -1,9 +1,20 @@
 use pyenv_python::python_path;
-use std::process::{exit, Command};
+use std::process::exit;
 use std::env;
 
-/// Run the current `python` (as determined by `pyenv`) with the given args.
+/// Run the current `python` (as determined by `pyenv`) with the given args (using exec::Command).
+#[cfg(unix)]
 fn main() {
+    let path = python_path().expect("python not found");
+    let err = exec::execvp(path, env::args_os());
+    eprintln!("{:?}", err);
+    exit(1);
+}
+
+/// Run the current `python` (as determined by `pyenv`) with the given args (using std::process::Command).
+#[cfg(not(unix))]
+fn main() {
+    use std::process::Command;
     let path = python_path().expect("python not found");
     let status = Command::new(path)
         .args(env::args_os().skip(1))
