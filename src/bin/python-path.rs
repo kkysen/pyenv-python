38f44285393e57f793f5dbc10b64c3fdacb73b41
Khyber Sen <kkysen@gmail.com>
2020-05-17 02:31:33 -0400

    Got the `python` wrapper to work using std::process::Command. Using execve() would be better, but it doesn't seem to be trivially available on Windows.

--- src/bin/python-path.rs
+++ src/bin/python-path.rs
@@ -2,6 +2,7 @@ use pyenv_python::python_path;
 use std::process::exit;
 use print_bytes::println_bytes;
 
+/// Print the current `python` path as determined by `pyenv`.
 fn main() {
     let status = match python_path() {
         Some(path) => {
--- src/bin/python.rs
+++ src/bin/python.rs
@@ -2,10 +2,11 @@ use pyenv_python::python_path;
 use std::process::{exit, Command};
 use std::env;
 
+/// Run the current `python` (as determined by `pyenv`) with the given args.
 fn main() {
     let path = python_path().expect("python not found");
     let status = Command::new(path)
-        .args(env::args_os())
+        .args(env::args_os().skip(1))
         .status()
         .expect("failed to run python subprocess")
         .code().unwrap_or_default();
